632f218b586e5174fba3b9d220455806ac8430ff
Igor-prog41 <61401897+Igor-prog41@users.noreply.github.com>
2022-02-09 08:14:36 -0700

    function repeatStr  (n, s)

--- codewars1.js
+++ codewars1.js
@@ -3,3 +3,10 @@ function makeNegative(num) {
   num>0? out = -num :out = num;
   return out;
 }
+function repeatStr  (n, s) {
+  let out = "";
+  for (let i = 0; i < n; i++) {
+    out += s;
+  }
+  return out;
+}
